9d70ffb20ec442b1aedd6b02ac53cfc65860e8d9
YuriMo2 <yuri.mo2@gmail.com>
2014-05-31 20:10:49 -0300

    Tests not working, deleted them

--- Trabalho2/main.c
+++ Trabalho2/main.c
@@ -1,7 +1,29 @@
 #include <convert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 80
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	char * coluna;
+	FILE * dumb;
+
+	dumb = fopen("dumb_file", "r");
+
+	coluna = (char *)calloc(SIZE, sizeof(char));
+	if (coluna == NULL)
+	{
+		printf("Tiranossauro aleijado! ABORTAR!\n");
+		return 0;
+	}
+
+	for (int i = 0; i < SIZE; ++i)
+	{
+		fscanf(dumb, "%c", &coluna[i]);
+	}
+
+	tradicional(coluna, SIZE);
+
 	return 0;
 }
