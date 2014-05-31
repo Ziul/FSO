ae501a6c7a24710bfd299d4aa66781ca23ee42cd
Ziul <ziuloliveira@gmail.com>
2014-05-31 20:49:37 -0300

    Don’t even try to refactor it.

--- Trabalho2/convert.c
+++ Trabalho2/convert.c
@@ -17,35 +17,59 @@ replace_char(char c1, char c2, char* data, unsigned int* size)
 }
 
 char*
-replace_string(char* c1, char* c2, char* data, unsigned int *size)
+replace_string(char* c1, char c2, char* data, unsigned int *size)
 {
 
 	unsigned int new_size = (*size)*2;
 	char* new_data = (char*) malloc(sizeof(char*)*new_size);
 	unsigned int i=0;
 
-	for (i=0;i<*size*2; i++)
+	for (i=0;i<*size; i++)
 	{
 		if ( (data[i]== c1[0]) && (data[i+1]== c1[1]) )
 		{
-			new_data[i]=c2[0];
-			new_data[++i]=c2[1];
+			new_data[i]=c2;
+			i++;
 		}
 		else
 			new_data[i] = data[i];
 	}
 
-	*size = new_size;
-
+	*size = i+1;
 	return new_data;
 }
+
+char* 
+reorder_lines(char* data, unsigned int *size)
+{
+	unsigned int new_size = (*size)*2;
+	char* new_data = (char*) malloc(sizeof(char*)*new_size);
+	unsigned int i=0;
+
+	for (i=0;i<*size; i++)
+	{
+		if ((i%NEW_BREAK_LINE)==0)
+		{
+			new_data[i]='\n';
+			i++;
+		}
+		new_data[i] = data[i];
+	}
+	new_data[++i] = ' ';
+	new_data[++i] = '\0';
+
+	*size = i+1;
+	return new_data;
+}
+
 
  char* 
  tradicional(char* data, unsigned int *size)
  {
 
- 	replace_char('\n',' ',data,size);
- 	replace_string("**","#.",data,size);
+ 	data = replace_char('\n',' ',data,size);
+ 	data = replace_string("**",'#',data,size);
+ 	data = reorder_lines(data, size);
 
  	return data;
  }
--- Trabalho2/convert.h
+++ Trabalho2/convert.h
@@ -2,6 +2,8 @@
 #ifndef __CONVERT__H__
 #define __CONVERT__H__
 
+#define NEW_BREAK_LINE 125
+
  char* 
  tradicional(char* data, unsigned int* size);
 
